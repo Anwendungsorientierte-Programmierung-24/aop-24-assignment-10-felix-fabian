025e6aaa00ebb4c794ccfdb26df36b601a3c9b4c
Felix Braun <167249862+braun-felix@users.noreply.github.com>
2024-07-10 20:36:35 +0200

    Add feature flags class.

---
+++ ur_place/lib/test/feature_flags.dart
@@ -0,0 +1,11 @@
+class FeatureFlags {
+  // Description: Start program with debug page to directly navigate to pages.
+  // Dependencies: none.
+  // Default: false.
+  static const enableDebugPage = true;
+
+  // Description: Placeholder.
+  // Dependencies: <dependencies>.
+  // Default: <bool>.
+  static const enableFeatureName = true;
+}
