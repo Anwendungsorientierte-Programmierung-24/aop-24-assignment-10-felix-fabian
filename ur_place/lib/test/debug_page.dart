3411848a2497cc2fc1f233e400c7bd124378257f
braun-felix <167249862+braun-felix@users.noreply.github.com>
2024-07-11 19:55:54 +0200

    Add FeatureFlags and Management. (#7)
    * DebugPage: Show all feature flags and their states.
    
    * FeatureFlagManager: Create class to store and manage FeatureFlags.
    
    * FeatureFlagsPage: Create page to change feature flags at runtime
    
    * Main: Update check to use FFManager.
    
    * Rename FeatureFlagManager to FFManager.

--- ur_place/lib/test/debug_page.dart
+++ ur_place/lib/test/debug_page.dart
@@ -4,16 +4,28 @@ import 'package:flutter/material.dart';
 import 'package:ur_place/pages/home_page.dart';
 import 'package:ur_place/pages/login_page.dart';
 import 'package:ur_place/pages/register_page.dart';
-import 'package:ur_place/test/feature_flags.dart';
+import 'package:ur_place/test/feature_flag_manager.dart';
+import 'package:ur_place/test/feature_flags_page.dart';
 
-class DebugPage extends StatelessWidget {
+class DebugPage extends StatefulWidget {
   const DebugPage({super.key});
 
+  @override
+  State<DebugPage> createState() => _DebugPageState();
+}
+
+class _DebugPageState extends State<DebugPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('⚠️ Debug Page'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.bug_report),
+            onPressed: () => setState(() {}),
+          )
+        ],
       ),
       body: Center(
         child: Column(
@@ -49,10 +61,17 @@ class DebugPage extends StatelessWidget {
             ),
             SizedBox(height: 10),
             Column(
-              children: [
-                Text('FeatureFlags state:'),
-                Text('enableDebugPage: ${FeatureFlags.enableDebugPage}'),
-              ],
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[Text('🚩 FeatureFlags:')] +
+                  List.generate(
+                    FFManager.getAllFeatureStates().length,
+                    (index) => GestureDetector(
+                      onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => FeatureFlagsPage())),
+                      child: Text(
+                        FFManager.getAllFeatureStates()[index],
+                      ),
+                    ),
+                  ),
             ),
           ],
         ),
