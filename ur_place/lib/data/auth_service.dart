c771d9e75f499c8bbec536f6a87fe206a9aa1938
Felix Braun <167249862+braun-felix@users.noreply.github.com>
2024-07-11 23:02:31 +0200

    AuthService: Add todos.

--- ur_place/lib/data/auth_service.dart
+++ ur_place/lib/data/auth_service.dart
@@ -1,13 +1,28 @@
 import "package:firebase_auth/firebase_auth.dart";
 
 class AuthService {
+  
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
   Future<UserCredential> signInWithEmailAndPassword(String email, String password) async {
+  
     try {
-      UserCredential userCredential = await _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
+      UserCredential userCredential = await _firebaseAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      
       return userCredential;
+    
     } on FirebaseAuthException catch (error) {
       throw Exception(error.code);
     }
+
   }
+
+  // TODO(aleksicf): Implement these methods.
+  // createUserWithEmailAndPassword(email, password)
+  // signOut()
+  // sendPasswordResetEmail(email)
+  
 }
