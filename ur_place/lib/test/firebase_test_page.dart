2dcc1462ee4c739284b09532a56ab20a0885a972
braun-felix <167249862+braun-felix@users.noreply.github.com>
2024-07-11 22:55:53 +0200

    Add FirebaseTestPage. (#11)
    * FirebaseTestPage: Print debug info.
    
    * Main: Gate firebase init with feature flag (enableFirebase).
    
    * FFManager: Fix clearing overrides.
    
    * FeatureFlagsPage: Allow clearing overrides.
    
    * FirebaseTestPage: Add refresh button.

--- ur_place/lib/test/firebase_test_page.dart
+++ ur_place/lib/test/firebase_test_page.dart
@@ -19,25 +19,37 @@ class _FireBaseTestPageState extends State<FireBaseTestPage> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-    
       appBar: AppBar(
         title: const Text("⚠️ Debug: Firebase"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.refresh),
+            onPressed: () => setState(() {}),
+          ),
+        ],
       ),
-    
       body: FutureBuilder(
-    
         future: _initialization,
-    
         builder: (context, snapshot) {
-
+          
           if (snapshot.hasError) {
-            return const Center(child: Text("Error initializing Firebase"));
+            return Center(child: Text('Error initializing Firebase\n${snapshot.error.toString()}'));
           }
 
           if (snapshot.connectionState == ConnectionState.done) {
-            return const Center(child: Text("Firebase is successfully connected!"));
+            
+            final values = snapshot.data!.options.asMap;
+
+            return Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text('Firebase is successfully connected! ✅\n'),
+                  ...values.entries.map((entry) => Text('${entry.key}: ${entry.value}\n')),
+                ],
+              ),
+            );
           }
 
           return const Center(child: CircularProgressIndicator());
