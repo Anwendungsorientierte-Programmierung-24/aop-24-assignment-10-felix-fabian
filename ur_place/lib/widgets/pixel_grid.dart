f6507afaff3e98bd0899a744480f061a7e9e7a81
Felix Braun <167249862+braun-felix@users.noreply.github.com>
2024-07-12 02:02:40 +0200

    PixelGrid: Make tapped pixels change color.

--- ur_place/lib/widgets/pixel_grid.dart
+++ ur_place/lib/widgets/pixel_grid.dart
@@ -10,31 +10,32 @@ class PixelGrid extends StatefulWidget {
 }
 
 class _PixelGridState extends State<PixelGrid> {
-  
   static int size = 10;
 
-  List<Widget> _pixels = List.generate(
-    size * size,
-    (index) => GestureDetector(
-      onTap: () => debugPrint('Hit Row ${index~/size}, Column ${index%size}'),
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.black, width: 0.5),
-          color: Colors.red,
-        ),
-      ),
-    ),
-  );
+  List<Color> _pixelColors = List.generate(100, (index) => Colors.red);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       constraints: const BoxConstraints.expand(height: 300, width: 300), // Visual grid container size
       child: GridView.builder(
-        itemCount: _pixels.length,
+        itemCount: _pixelColors.length,
         physics: const NeverScrollableScrollPhysics(),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: size),
-        itemBuilder: (context, index) => _pixels[index],
+        itemBuilder: (context, index) => GestureDetector(
+          onTap: () {
+            debugPrint('Hit Row ${index ~/ size}, Column ${index % size}');
+            setState(() {
+              _pixelColors[index] = Colors.black;
+            });
+          },
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black, width: 0.5),
+              color: _pixelColors[index],
+            ),
+          ),
+        ),
       ),
     );
   }
