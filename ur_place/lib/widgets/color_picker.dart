a5fa88b8326d0e36d66be912d848469b3358ed31
braun-felix <167249862+braun-felix@users.noreply.github.com>
2024-07-18 19:07:06 +0200

    Fix canvas flicker on interaction. (#22)
    * PlaceView: Draw canvas with local state while we're waiting for update.

--- ur_place/lib/widgets/color_picker.dart
+++ ur_place/lib/widgets/color_picker.dart
@@ -1,28 +1,38 @@
 import 'package:flutter/material.dart';
 
-class ColorPicker extends StatelessWidget {
+class ColorPicker extends StatefulWidget {
   final ValueChanged _changed;
-  
-  static final List<Color> colors = [Colors.black, Colors.red, Colors.green, Colors.blue, Colors.yellow, Colors.orange];
+  late int _currentColor;
 
+  static final List<Color> colors = [Colors.white, Colors.black, Colors.red, Colors.green, Colors.blue, Colors.yellow, Colors.orange];
 
-  ColorPicker({required ValueChanged setBrushColor, super.key}) : _changed = setBrushColor;
+  ColorPicker({required ValueChanged setBrushColor, required int currentColor, super.key}) : _changed = setBrushColor, _currentColor = currentColor;
+
+  @override
+  State<ColorPicker> createState() => _ColorPickerState();
+}
+
+class _ColorPickerState extends State<ColorPicker> {
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        const Text('TODO(fbraun): Color Swatch Temporary UI'),
-        // TODO(fbraun): Update UI to be color grid.
         SingleChildScrollView(
           scrollDirection: Axis.horizontal,
           child: Row(
             mainAxisSize: MainAxisSize.min,
             children: List.generate(
-              colors.length,
+              ColorPicker.colors.length,
               (index) => ElevatedButton(
-                style: ElevatedButton.styleFrom(backgroundColor: colors[index % colors.length]),
-                onPressed: () => _changed(colors[index]),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: ColorPicker.colors[index % ColorPicker.colors.length],
+                  foregroundColor: index == widget._currentColor ? Colors.yellow : null,
+                ),
+                onPressed: () {
+                  setState(() => widget._currentColor = index); // We store the current color locally so we can update our visual selection.
+                  widget._changed(ColorPicker.colors[index]);
+                },
                 child: Text('Color $index'),
               ),
             ),
