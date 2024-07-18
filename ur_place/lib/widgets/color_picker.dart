58b1404f242492da9c1c95bb952ff776fec5a951
FabiSVM <131012352+AleksicF@users.noreply.github.com>
2024-07-18 22:59:35 +0200

    remove bpttom overflow

--- ur_place/lib/widgets/color_picker.dart
+++ ur_place/lib/widgets/color_picker.dart
@@ -28,33 +28,37 @@ class ColorPicker extends StatefulWidget {
 class _ColorPickerState extends State<ColorPicker> {
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      shrinkWrap: true,
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3, // Number of columns
-        childAspectRatio: 1.0, // Aspect ratio of each item
-      ),
-      itemCount: ColorPicker.colors.length,
-      itemBuilder: (context, index) {
-        return GestureDetector(
-          onTap: () {
-            setState(() => widget._currentColor = index); // We store the current color locally so we can update our visual selection.
-            widget._changed(ColorPicker.colors[index]);
-          },
-          child: Container(
-            padding: EdgeInsets.all(20),
-            margin: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: ColorPicker.colors[index],
-              border: Border.all(
-                color: index == widget._currentColor ? Colors.yellow : Colors.transparent,
-                width: 3,
+    return Container(
+      height: 100, // Adjust height as needed to fit within the layout
+      child: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 4, // Number of columns
+          childAspectRatio: 2.0, // Aspect ratio of each item
+          mainAxisSpacing: 5,
+          crossAxisSpacing: 5,
+        ),
+        itemCount: ColorPicker.colors.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              setState(() => widget._currentColor = index); // We store the current color locally so we can update our visual selection.
+              widget._changed(ColorPicker.colors[index]);
+            },
+            child: Container(
+              padding: EdgeInsets.all(5),
+              margin: EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                color: ColorPicker.colors[index],
+                border: Border.all(
+                  color: index == widget._currentColor ? Colors.yellow : Colors.transparent,
+                  width: 2,
+                ),
+                borderRadius: BorderRadius.circular(5),
               ),
-              borderRadius: BorderRadius.circular(5),
             ),
-          ),
-        );
-      },
+          );
+        },
+      ),
     );
   }
 }
