692ae5f6d1565cb7b9e9a3124b46aa375c4e1879
AleksicF <131012352+AleksicF@users.noreply.github.com>
2024-07-19 22:16:46 +0200

    Update ColorPicker UI to grid. (#23)
    * added active color
    
    * added active color v2
    
    * added Container with GestureDetector
    
    * added a grid view for buttons
    
    * remove bpttom overflow
    
    * changed aspect of ratio
    
    * added const where helpful
    
    ---------
    
    Co-authored-by: braun-felix <167249862+braun-felix@users.noreply.github.com>

--- ur_place/lib/widgets/color_picker.dart
+++ ur_place/lib/widgets/color_picker.dart
@@ -1,44 +1,64 @@
 import 'package:flutter/material.dart';
 
 class ColorPicker extends StatefulWidget {
-  final ValueChanged _changed;
+
+  final ValueChanged<Color> _changed;
   late int _currentColor;
 
-  static final List<Color> colors = [Colors.white, Colors.black, Colors.red, Colors.green, Colors.blue, Colors.yellow, Colors.orange];
+  static final List<Color> colors = [Colors.white, Colors.black, Colors.red, Colors.green, Colors.blue, Colors.yellow, Colors.orange, Colors.purple];
 
-  ColorPicker({required ValueChanged setBrushColor, required int currentColor, super.key}) : _changed = setBrushColor, _currentColor = currentColor;
+  ColorPicker({
+    required ValueChanged<Color> setBrushColor,
+    required int currentColor,
+    super.key,
+  })  : _changed = setBrushColor,
 
   @override
   State<ColorPicker> createState() => _ColorPickerState();
 }
 
 class _ColorPickerState extends State<ColorPicker> {
-
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: List.generate(
-              ColorPicker.colors.length,
-              (index) => ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: ColorPicker.colors[index % ColorPicker.colors.length],
-                  foregroundColor: index == widget._currentColor ? Colors.yellow : null,
+    return Container(
+      height: 150,
+      child: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 4, // Number of columns
+          childAspectRatio: 2.0, // appearence of the boxes
+          mainAxisSpacing: 5,
+          crossAxisSpacing: 5,
+        ),
+        itemCount: ColorPicker.colors.length,
+        itemBuilder: (context, index) {
+          Color borderColor;
+          if (index == widget._currentColor) {
+            // shows which color is active rn
+            borderColor = Colors.grey;
+          } else {
+            borderColor = Colors.transparent;
+          }
+
+          return GestureDetector(
+            onTap: () {
+              setState(() => widget._currentColor = index); // We store the current color locally so we can update our visual selection
+              widget._changed(ColorPicker.colors[index]);
+            },
+            child: Container(
+              padding: const EdgeInsets.all(5),
+              margin: const EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                color: ColorPicker.colors[index],
+                border: Border.all(
+                  color: borderColor,
+                  width: 2,
                 ),
-                onPressed: () {
-                  setState(() => widget._currentColor = index); // We store the current color locally so we can update our visual selection.
-                  widget._changed(ColorPicker.colors[index]);
-                },
-                child: Text('Color $index'),
+                borderRadius: BorderRadius.circular(5),
               ),
             ),
-          ),
-        ),
-      ],
+          );
+        },
+      ),
     );
   }
 }
