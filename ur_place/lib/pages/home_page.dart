1b360b4594a9989d89870b3ef03166471b814714
Felix Braun <167249862+braun-felix@users.noreply.github.com>
2024-07-21 23:10:48 +0200

    HomePage: Add logout button.

--- ur_place/lib/pages/home_page.dart
+++ ur_place/lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:ur_place/views/place_view.dart';
 
@@ -11,13 +12,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('HomePage')),
+      appBar: AppBar(
+        title: Text('HomePage'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.login),
+            onPressed: () => _auth.signOut(),
+          ),
+        ],
+      ),
       body: Column(
         children: [
-          const Text('TODO(fbraun): Not final UI.', style: TextStyle(color: Colors.red,fontSize: 20)),
+          const Text('TODO(fbraun): Not final UI.', style: TextStyle(color: Colors.red, fontSize: 20)),
           SizedBox(height: 150),
           Center(
             child: PlaceView(),
